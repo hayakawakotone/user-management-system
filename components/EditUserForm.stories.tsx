22a1ba3b58f3f7fb83eb1ba577171f7668182efd
hayakawakotone <kotone.hayakawa@fox-hound.jp>
2025-06-11 11:21:32 +0900

    2-2完了

--- components/EditUserForm.stories.tsx
+++ components/EditUserForm.stories.tsx
@@ -2,10 +2,16 @@
 
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import EditUserForm from "../../components/EditUserForm";
+import EditUserForm from "../components/EditUserForm";
 
 // TODO: メタデータ
+const meta: Meta<typeof EditUserForm> = {
+  title: "Components/EditUserForm", // Storybook上の表示パス
+  component: EditUserForm,
+};
+export default meta;
 // TODO: ストーリーの定義
+type Story = StoryObj<typeof EditUserForm>;
 
 export const Default: Story = {
   args: {
