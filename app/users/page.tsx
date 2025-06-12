286cb66f48b7c449dcf7a126220511acad270366
hayakawakotone <kotone.hayakawa@fox-hound.jp>
2025-06-12 11:42:01 +0900

    1-3　完了

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,50 +1,24 @@
-"use client";
-
-import { Alert, Box, CircularProgress, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import UserCard from "../../components/UserCard";
 import { User } from "../../types/User";
 import { fetchUsers } from "../../utils/api";
+// 作成済のUserListコンポーネントをインポート（表示に使うやつ）
+import UserList from "../../components/UserList";
 
-const UsersPage: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const data = await fetchUsers();
-        console.log(data);
-        setUsers(data);
-      } catch (err) {
-        setError("ユーザーの取得に失敗しました。" + err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getUsers();
-  }, []);
-
-  if (loading) {
-    return <CircularProgress />;
-  }
-
-  if (error) {
-    return <Alert severity="error">{error}</Alert>;
-  }
+   // fetchUsers()でAPIからユーザーデータを取得して、usersという変数に入れる
+  const UserListPage = async () => {
+ 
+  const users: User[] = await fetchUsers();
 
   return (
-    <Box>
-      <Typography variant="h4" gutterBottom>
+    <Container maxWidth="md" sx={{ mt: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
         ユーザー一覧
       </Typography>
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
-    </Box>
+      <UserList users={users} />
+    </Container>
   );
+
 };
 
-export default UsersPage;
+export default UserListPage;
