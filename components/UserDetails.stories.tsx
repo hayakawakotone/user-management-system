44665607b7933a01e7053750d9dea6a54eec3362
hayakawakotone <kotone.hayakawa@fox-hound.jp>
2025-06-12 16:37:07 +0900

    3-2完了

---
+++ components/UserDetails.stories.tsx
@@ -0,0 +1,26 @@
+// components/UserDetails.stories.tsx
+
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import UserDetails from "../components/UserDetails";
+
+const meta: Meta<typeof UserDetails> = {
+  title: "Components/UserDetails", 
+  component: UserDetails,
+};
+export default meta;
+// ストーリーの定義
+type Story = StoryObj<typeof UserDetails>;
+
+export const Default: Story = {
+  args: {
+    // 仮データ
+    user:{
+        id:1,
+        name:'テスト',
+        email:'test',
+        role:'aaa',
+        deleted:false,
+    }
+  },
+};
