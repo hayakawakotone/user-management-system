55cc1bf1946ba62cbff90efd5079a6044cc3db05
hayakawakotone <kotone.hayakawa@fox-hound.jp>
2025-06-13 16:06:49 +0900

    2-1　完了

--- components/parts/CustomCard.tsx
+++ components/parts/CustomCard.tsx
@@ -3,8 +3,10 @@
 import React from "react";
 import { Card, CardContent, Typography, CardActions } from "@mui/material";
 
-// TODO: インターフェースを修正
+// 追記：インターフェースを修正
 interface CustomCardProps {
+  title: string;
+  description: string;
   actions?: React.ReactNode;
 }
 
@@ -16,9 +18,13 @@ const CustomCard: React.FC<CustomCardProps> = ({
   return (
     <Card sx={{ minWidth: 275, mb: 2 }}>
       <CardContent>
-        {/*
-		      TODO: [titel]と[description]を表示
-		     */}
+        <Typography variant="h5" component={"div"} gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {/*  2-3でよこ並びになるので直すかも*/}
+          {description}
+        </Typography>
       </CardContent>
       {actions && <CardActions>{actions}</CardActions>}
     </Card>
